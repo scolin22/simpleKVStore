a79b545aa3fcd2f011d359831185a71cff44dcb6
Colin Stone <colin@bunnylaundering.com>
2016-01-17 17:17:12 -0800

    Adding JAR/tests

--- src/com/scolin22/cpen431/A2/Main.java
+++ src/com/scolin22/cpen431/A2/Main.java
@@ -3,6 +3,7 @@ package com.scolin22.cpen431.A2;
 public class Main {
 
     public static void main(String[] args) {
+        //TODO: accept arguments at least for port, timeout
         if (args.length != 0) {
             System.out.println("Invalid number of arguments: e.g. java -jar simpleKVStore.jar");
             return;
